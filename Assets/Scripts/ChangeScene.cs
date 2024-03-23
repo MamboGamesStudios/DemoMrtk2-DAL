335334ef2473d8536fe93e200b3c3416cdd5334a
MamboGamesStudios <davidgarciadiaz@alumno.ieselrincon.es>
2024-03-23 21:49:51 +0000

    Preparado Entregar

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -8,7 +8,7 @@ public class ChangeScene : MonoBehaviour
 
     public void GoToMenu()
     {
-        SCManager.instance.LoadScene("Menu");
+        SCManager.instance.LoadScene("Intro");
     }
 
     public void GoToGame()
